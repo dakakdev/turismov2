94b66a7906095c31938164f2f677fff74a58bf7e
Dakakdev <dakakdev@gmail.com>
2025-08-17 20:26:00 -0400

    Page Complete 0.1

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.SUPABASE_URL!;
-const supabaseAnonKey = import.meta.env.SUPABASE_ANON_KEY!;
+const supabaseUrl = import.meta.env.SUPABASE_URL;
+const supabaseKey = import.meta.env.SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-  auth: { persistSession: false },
-});
+export const supabase = createClient(supabaseUrl, supabaseKey);
