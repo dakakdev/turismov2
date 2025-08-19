5be1881d5af9ddef9df88237ac0175aef838371c
Dakakdev <dakakdev@gmail.com>
2025-08-19 11:06:42 -0400

    Page Complete 0.3 Sitemaps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://mannytoursrd.com/",
-  output: "server",
-  adapter: vercel({}),
+  output: "static", // 👈 importante
+  adapter: vercel(),
   integrations: [
     sitemap({
       filter: (page) => !page.includes("/api") && !page.includes("/_image"),
-      sitemap: "/sitemap.xml", // 👈 nombre fijo
     }),
   ],
   vite: {
